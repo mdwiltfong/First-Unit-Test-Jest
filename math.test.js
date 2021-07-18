1a1ad97edb0db88e5b72f3cd1108666d93681fdd
mdwiltfong <wiltfong.michael1@gmail.com>
2021-07-18 02:13:45 -0400

    Update math.test.js

--- math.test.js
+++ math.test.js
@@ -1,14 +1,38 @@
 const { sum, mul, sub, div } = require("./math")
+const mathjs = require('./math')
 
-test("Adding 1 + 1 equals 2", () => {
-  expect(sum(1, 1)).toBe(2)
+describe('Adding',()=>{
+    test("Adding 1 + 1 equals 2", () => {
+        expect(sum(1, 1)).toBe(2)
+      })
 })
-test("Multiplying 1 * 1 equals 1", () => {
-  expect(mul(1, 1)).toBe(1)
+describe('Multiplying',()=>{
+    test("Multiplying 1 * 1 equals 1", () => {
+        expect(mul(1, 1)).toBe(1)
+      })
 })
-test("Subtracting 1 - 1 equals 0", () => {
+describe('Subtracting',()=>{
+    test("Subtracting 1 - 1 equals 0", () => {
   expect(sub(1, 1)).toBe(0)
 })
-test("Dividing 1 / 1 equals 1", () => {
-  expect(div(1, 1)).toBe(1)
 })
+describe('Dividing',()=>{
+    test("Dividing 1 / 1 equals 1", () => {
+        expect(div(1, 1)).toBe(1)
+      })
+})
+
+describe(`Math.js using spy packages`,()=>{
+    test(`The mathjs log function`, () => {
+        const spy = jest.spyOn(mathjs, 'log')
+        const result = mathjs.log(10000, 10)
+      
+        expect(mathjs.log).toHaveBeenCalled()
+        expect(mathjs.log).toHaveBeenCalledWith(10000, 10)
+      })
+
+})
+
+
+
+
